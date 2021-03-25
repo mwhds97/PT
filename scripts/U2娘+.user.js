1e858a77e5d57def32df226f451f16028af86255
Optimus Primal <32876945+mwhds97@users.noreply.github.com>
2021-03-25 21:50:55 +0800

    Add files via upload

--- scripts/U2娘+.user.js
+++ scripts/U2娘+.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         U2娘+
-// @version      1.5
+// @version      1.6
 // @author       mwhds97
 // @match        *.u2.dmhy.org/*
 // @grant        none
@@ -20,6 +20,66 @@
     }, false);
   }
 
+  var buttons = [{"name": "income", "value": "赚分速度", "word": "我的赚分速度"},
+                 {"name": "myuc", "value": "我的UC", "word": "我的UCoin"},
+                 {"name": "alluc", "value": "全站UC", "word": "全站UC存量"},
+                 {"name": "rich", "value": "比我壕", "word": "U2有多少人比我壕"},
+                 {"name": "afford", "value": "买得起药", "word": "多少人买得起邀请"},
+                 {"name": "ratio", "value": "分享率", "word": "我的分享率"},
+                 {"name": "real", "value": "实际分享率", "word": "我的实际分享率"},
+                 {"name": "sign", "value": "签名条", "word": "签名条"},
+                 {"name": "skill", "value": "技能", "word": "我的技能"},
+                 {"name": "photo", "value": "头像", "word": "看看我的头像"},
+                 {"name": "so", "value": "取向", "word": "我的取向"},
+                 {"name": "harem", "value": "后宫", "word": "我的后宫"},
+                 {"name": "power", "value": "战斗力", "word": "我的战斗力"},
+                 {"name": "ohayo", "value": "早安", "word": "早安"},
+                 {"name": "oyasumi", "value": "晚安", "word": "晚安"},
+                 {"name": "boy", "value": "男生", "word": "U2有多少男生"},
+                 {"name": "girl", "value": "女生", "word": "U2有多少女生"},
+                 {"name": "newhalf", "value": "伪娘", "word": "U2有多少伪娘"},
+                 {"name": "comfort", "value": "求安慰", "word": "求安慰"},
+                 {"name": "daddy", "value": "求包养", "word": "求包养"},
+                 {"name": "merge", "value": "求合体", "word": "求合体"},
+                 {"name": "date", "value": "求交往", "word": "求交往"},
+                 {"name": "sex", "value": "求交尾", "word": "求交尾"},
+                 {"name": "sm", "value": "求虐", "word": "求虐"},
+                 {"name": "train", "value": "求调教", "word": "求调教"},
+                 {"name": "invite", "value": "求邀请", "word": "求邀请"},
+                 {"name": "creampie", "value": "求中出", "word": "求中出"},
+                 {"name": "lollipop", "value": "棒棒糖", "word": "棒棒糖"},
+                 {"name": "transform", "value": "变身", "word": "变身"},
+                 {"name": "bkb", "value": "兵库北", "word": "兵库北"},
+                 {"name": "title", "value": "长标题", "word": "长标题"},
+                 {"name": "roll", "value": "抽种子", "word": "手气不错"},
+                 {"name": "comeout", "value": "出柜", "word": "出柜"},
+                 {"name": "gohome", "value": "带回家", "word": "跟我回家去"},
+                 {"name": "pour", "value": "倒茶", "word": "倒茶"},
+                 {"name": "creed", "value": "教义", "word": "教义"},
+                 {"name": "dignity", "value": "节操", "word": "掉节操"},
+                 {"name": "donate", "value": "捐赠", "word": "捐赠"},
+                 {"name": "loli", "value": "萝莉", "word": "萝莉"},
+                 {"name": "mahjong", "value": "麻将", "word": "麻将"},
+                 {"name": "moe", "value": "卖萌", "word": "卖个萌"},
+                 {"name": "head", "value": "摸摸头", "word": "摸摸头"},
+                 {"name": "mhsj", "value": "魔法少女", "word": "魔法少女"},
+                 {"name": "warm", "value": "暖被窝", "word": "暖被窝"},
+                 {"name": "pants", "value": "胖次", "word": "胖次"},
+                 {"name": "fall", "value": "平地摔", "word": "平地摔"},
+                 {"name": "choc", "value": "巧克力", "word": "巧克力"},
+                 {"name": "tea", "value": "请用茶", "word": "请用茶"},
+                 {"name": "body", "value": "身体检查", "word": "身体检查"},
+                 {"name": "bithday", "value": "生日", "word": "生日"},
+                 {"name": "world", "value": "世界线", "word": "世界线"},
+                 {"name": "model", "value": "手办", "word": "你的手办多少钱"},
+                 {"name": "rabbit", "value": "兔纸", "word": "兔纸"},
+                 {"name": "push", "value": "推倒", "word": "推倒"},
+                 {"name": "love", "value": "我爱你", "word": "我爱你"},
+                 {"name": "back", "value": "我回来了", "word": "我回来了"},
+                 {"name": "bath", "value": "洗澡", "word": "洗澡"},
+                 {"name": "play", "value": "羞耻play", "word": "羞耻play"},
+                 {"name": "eternal", "value": "+1秒", "word": "+1s"}];
+
   var shbox = document.getElementsByName("shbox")[0];
   for(var i in shbox.childNodes) {
     if(shbox.childNodes[i].value == "清除") {
@@ -27,64 +87,7 @@
       break;
     }
   }
-
-  CreateButton(shbox, i + 1, "income", "赚分速度", "我的赚分速度");
-  CreateButton(shbox, i + 2, "myuc", "我的UC", "我的UCoin");
-  CreateButton(shbox, i + 3, "alluc", "全站UC", "全站UC存量");
-  CreateButton(shbox, i + 4, "rich", "比我壕", "U2有多少人比我壕");
-  CreateButton(shbox, i + 5, "afford", "买得起药", "多少人买得起邀请");
-  CreateButton(shbox, i + 6, "ratio", "分享率", "我的分享率");
-  CreateButton(shbox, i + 7, "real", "实际分享率", "我的实际分享率");
-  CreateButton(shbox, i + 8, "sign", "签名条", "签名条");
-  CreateButton(shbox, i + 9, "skill", "技能", "我的技能");
-  CreateButton(shbox, i + 10, "photo", "头像", "看看我的头像");
-  CreateButton(shbox, i + 11, "so", "取向", "我的取向");
-  CreateButton(shbox, i + 12, "harem", "后宫", "我的后宫");
-  CreateButton(shbox, i + 13, "power", "战斗力", "我的战斗力");
-  CreateButton(shbox, i + 14, "ohayo", "早安", "早安");
-  CreateButton(shbox, i + 15, "oyasumi", "晚安", "晚安");
-  CreateButton(shbox, i + 16, "boy", "男生", "U2有多少男生");
-  CreateButton(shbox, i + 17, "girl", "女生", "U2有多少女生");
-  CreateButton(shbox, i + 18, "newhalf", "伪娘", "U2有多少伪娘");
-  CreateButton(shbox, i + 19, "comfort", "求安慰", "求安慰");
-  CreateButton(shbox, i + 20, "daddy", "求包养", "求包养");
-  CreateButton(shbox, i + 21, "merge", "求合体", "求合体");
-  CreateButton(shbox, i + 22, "date", "求交往", "求交往");
-  CreateButton(shbox, i + 23, "sex", "求交尾", "求交尾");
-  CreateButton(shbox, i + 24, "sm", "求虐", "求虐");
-  CreateButton(shbox, i + 25, "train", "求调教", "求调教");
-  CreateButton(shbox, i + 26, "invite", "求邀请", "求邀请");
-  CreateButton(shbox, i + 27, "creampie", "求中出", "求中出");
-  CreateButton(shbox, i + 28, "lollipop", "棒棒糖", "棒棒糖");
-  CreateButton(shbox, i + 29, "transform", "变身", "变身");
-  CreateButton(shbox, i + 30, "bkb", "兵库北", "兵库北");
-  CreateButton(shbox, i + 31, "title", "长标题", "长标题");
-  CreateButton(shbox, i + 32, "roll", "抽种子", "手气不错");
-  CreateButton(shbox, i + 33, "comeout", "出柜", "出柜");
-  CreateButton(shbox, i + 34, "gohome", "带回家", "跟我回家去");
-  CreateButton(shbox, i + 35, "pour", "倒茶", "倒茶");
-  CreateButton(shbox, i + 36, "creed", "教义", "教义");
-  CreateButton(shbox, i + 37, "dignity", "节操", "掉节操");
-  CreateButton(shbox, i + 38, "donate", "捐赠", "捐赠");
-  CreateButton(shbox, i + 39, "loli", "萝莉", "萝莉");
-  CreateButton(shbox, i + 40, "mahjong", "麻将", "麻将");
-  CreateButton(shbox, i + 41, "moe", "卖萌", "卖个萌");
-  CreateButton(shbox, i + 42, "head", "摸摸头", "摸摸头");
-  CreateButton(shbox, i + 43, "mhsj", "魔法少女", "魔法少女");
-  CreateButton(shbox, i + 44, "warm", "暖被窝", "暖被窝");
-  CreateButton(shbox, i + 45, "pants", "胖次", "胖次");
-  CreateButton(shbox, i + 46, "fall", "平地摔", "平地摔");
-  CreateButton(shbox, i + 47, "choc", "巧克力", "巧克力");
-  CreateButton(shbox, i + 48, "tea", "请用茶", "请用茶");
-  CreateButton(shbox, i + 49, "body", "身体检查", "身体检查");
-  CreateButton(shbox, i + 50, "bithday", "生日", "生日");
-  CreateButton(shbox, i + 51, "world", "世界线", "世界线");
-  CreateButton(shbox, i + 52, "model", "手办", "你的手办多少钱");
-  CreateButton(shbox, i + 53, "rabbit", "兔纸", "兔纸");
-  CreateButton(shbox, i + 54, "push", "推倒", "推倒");
-  CreateButton(shbox, i + 55, "love", "我爱你", "我爱你");
-  CreateButton(shbox, i + 56, "back", "我回来了", "我回来了");
-  CreateButton(shbox, i + 57, "bath", "洗澡", "洗澡");
-  CreateButton(shbox, i + 58, "play", "羞耻play", "羞耻play");
-  CreateButton(shbox, i + 59, "eternal", "+1秒", "+1s");
+  for(var j = 0; j < buttons.length; j++) {
+    CreateButton(shbox, i + j + 1, buttons[j].name, buttons[j].value, buttons[j].word);
+  }
 })();
