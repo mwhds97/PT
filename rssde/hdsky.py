270a6452fc6f4a0edb43b0349b068ed86af4e994
mwhds97 <cjctcj68@gmail.com>
2021-06-11 19:55:32 +0800

    Update rssde

--- rssde/hdsky.py
+++ rssde/hdsky.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#coding=utf-8
 
 import re
 import time
 
 import feedparser
+import requests
 import yaml
+from bs4 import BeautifulSoup
 from deluge_client import DelugeRPCClient
 
 
@@ -16,7 +18,7 @@ def print_t(text, eol="\n", sol="\x1b[2K"):
 
 
 def size_G(size_str):
-    size = re.match(r"(.*) ([KMGT])?i?B", size_str).groups()
+    size = re.match(r"(\d+(?:\.\d+)?)[\n\s]*([KMGT])?i?B", size_str).groups()
     if size[1] == None:
         return float(size[0]) / 1073741824
     elif size[1] == "K":
@@ -30,7 +32,7 @@ def size_G(size_str):
 
 
 def NewClient(config):
-    client = DelugeRPCClient(
+    return DelugeRPCClient(
         config["host"].split(":")[0],
         int(config["host"].split(":")[1]),
         config["user"],
@@ -38,37 +40,32 @@ def NewClient(config):
         True,
         False,
     )
-    return client
+
+
+def RemoveTorrent(torrent, info):
+    if torrent["hash"] in eod:
+        del eod[torrent["hash"]]
+    client.call("core.remove_torrent", torrent["hash"], True)
+    print_t("删除种子（{:.2f}GB），原因：{}".format(torrent["total_size"] / 1073741824, info))
 
 
 with open("hdsky.yaml", "r") as f:
     config = yaml.load(f, yaml.FullLoader)
 client = NewClient(config)
 client.connect()
+eod = {}
 while True:
     try:
-        torrents = client.call(
-            "core.get_torrents_status",
-            {},
-            ["hash", "total_size", "seeding_time", "tracker_status"],
-        )
-        print_t("连接正常", "\r")
-        currentTotalSize = 0
-        for t in list(torrents.values()):
-            if (
-                t["seeding_time"] >= config["str"] * t["total_size"] / 1073741824 * 60
-                or "registered" in t["tracker_status"]
-            ):
-                client.call("core.remove_torrent", t["hash"], True)
-                print_t("删除种子（{:.2f}GB）".format(t["total_size"] / 1073741824))
-            else:
-                currentTotalSize += t["total_size"] / 1073741824
         feed = feedparser.parse(config["rss"])
         entries = [
             {
+                "id": re.search(r"id=(\d+)", f["link"]).group(1),
+                "hash": f["id"],
                 "time": time.mktime(time.gmtime()) - time.mktime(f["published_parsed"]),
-                "size": size_G(re.match(r"^.*\[(.*)\]$", f["title"]).groups()[0]),
+                "size": size_G(re.search(r"\[(.*)\]$", f["title"]).group(1)),
                 "link": f["links"][1]["href"],
+                "free": False,
+                "end": None,
             }
             for f in feed["entries"]
         ]
@@ -80,23 +77,83 @@ while True:
             )
         )
         entries.sort(key=lambda e: e["size"], reverse=True)
+        if config["free"]:
+            response = requests.get(
+                "https://hdsky.me/torrents.php",
+                headers={
+                    "user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"
+                },
+                cookies=config["cookie"],
+                timeout=15,
+            )
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, "lxml")
+                rows = soup.find_all("tr", class_="progresstr")
+                for r in rows:
+                    id = re.search("id=(\d+)\D", str(r)).group(1)
+                    for e in entries:
+                        if e["id"] == id:
+                            if 'alt="Free"' in str(r) and not "即将结束" in str(r):
+                                e["free"] = True
+                                end = re.search(r"<b><span title=\"(.*?)\"", str(r))
+                                e["end"] = (
+                                    None
+                                    if end == None
+                                    else time.mktime(
+                                        time.strptime(end.group(1), "%Y-%m-%d %H:%M:%S")
+                                    )
+                                )
+                                if e["hash"] in eod:
+                                    eod[e["hash"]] = e["end"]
+                            else:
+                                if e["hash"] in eod:
+                                    eod[e["hash"]] = 0
+                            break
+            entries = list(filter(lambda e: e["free"], entries))
+    except:
+        pass
+    try:
+        torrents = client.call(
+            "core.get_torrents_status",
+            {},
+            ["hash", "total_size", "seeding_time", "tracker_status"],
+        )
+        print_t("连接正常", "\r")
+        currentTotalSize = 0
+        for t in list(torrents.values()):
+            if (
+                config["free"]
+                and t["seeding_time"] == 0
+                and eod[t["hash"]] != None
+                and eod[t["hash"]] - time.mktime(time.localtime()) <= config["interval"]
+            ):
+                RemoveTorrent(t, "免费到期")
+            elif t["seeding_time"] >= config["str"] * t["total_size"] / 1073741824 * 60:
+                RemoveTorrent(t, "做种时间达到要求")
+            elif "registered" in t["tracker_status"]:
+                RemoveTorrent(t, "种子被撤回")
+            else:
+                currentTotalSize += t["total_size"] / 1073741824
         for e in entries:
-            if currentTotalSize + e["size"] <= config["space"]:
-                try:
-                    client.call(
-                        "core.add_torrent_url",
-                        e["link"],
-                        {"download_location": config["path"]},
-                    )
-                    currentTotalSize += e["size"]
-                    print_t(
-                        "添加种子（{:.2f}GB），总体积 {:.2f}GB".format(
-                            e["size"], currentTotalSize
+            if config["free"] and e["free"] or not config["free"]:
+                if currentTotalSize + e["size"] <= config["space"]:
+                    try:
+                        if config["free"]:
+                            eod[e["hash"]] = e["end"]
+                        client.call(
+                            "core.add_torrent_url",
+                            e["link"],
+                            {"download_location": config["path"]},
                         )
-                    )
-                except Exception as e:
-                    if not "Torrent already in session" in repr(e):
-                        raise Exception
+                        currentTotalSize += e["size"]
+                        print_t(
+                            "添加种子（{:.2f}GB），总体积 {:.2f}GB".format(
+                                e["size"], currentTotalSize
+                            )
+                        )
+                    except Exception as e:
+                        if not "Torrent already in session" in repr(e):
+                            raise Exception
         time.sleep(config["interval"])
     except KeyboardInterrupt:
         client.disconnect()
