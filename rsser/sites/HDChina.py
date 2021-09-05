8a11df9fb32ec4d5f54a79c14abf5a12af533ce5
mwhds97 <cjctcj68@gmail.com>
2021-09-05 17:34:18 +0800

    Update rsser

--- rsser/sites/HDChina.py
+++ rsser/sites/HDChina.py
@@ -1,3 +1,4 @@
+import json
 import re
 import time
 
@@ -28,8 +29,9 @@ def HDChina(config):
         }
         for entry in feed["entries"]
     }
+    session = requests.session()
     for web in config["HDChina"]["web"]:
-        response = requests.get(
+        response = session.get(
             web,
             headers={"user-agent": config["HDChina"]["user_agent"]},
             cookies=config["HDChina"]["cookies"],
@@ -41,12 +43,15 @@ def HDChina(config):
             rows = soup.find("table", class_="torrent_list").find_all(
                 "tr", recursive=False
             )
-            if rows == []:
+            csrf = soup.find("meta", {"name": "x-csrf"})["content"]
+            if rows == [] or len(csrf) != 40:
                 raise Exception
+            ids = []
             for row in rows[1:]:
                 cols = row.find_all("td", recursive=False)
                 if len(cols) >= 9:
                     id = re.search("id=(\d+)", str(cols[1])).group(1)
+                    ids.append(("ids[]", id))
                     if id in torrents:
                         web_info = {
                             "free": False,
@@ -57,7 +62,6 @@ def HDChina(config):
                             "leecher": -1,
                             "snatch": -1,
                         }
-                        # TODO: Promotion
                         if cols[1].find("div", class_="progress") != None:
                             web_info["downloaded"] == True
                         web_info["seeder"] = int(re.sub("\D", "", cols[5].text))
@@ -67,6 +71,41 @@ def HDChina(config):
         else:
             raise Exception
         time.sleep(1)
+        response = session.post(
+            url="https://hdchina.org/ajax_promotion.php",
+            headers={
+                "User-Agent": config["HDChina"]["user_agent"],
+                "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+            },
+            data=ids + [("csrf", csrf)],
+            cookies=config["HDChina"]["cookies"],
+            proxies=config["HDChina"]["proxies"],
+            timeout=config["HDChina"]["web_timeout"],
+        )
+        if response.status_code == 200:
+            pro_info = json.loads(response.text)
+            if pro_info["status"] != 200 or pro_info["message"] == {}:
+                raise Exception
+            for id, state in pro_info["message"].items():
+                if (
+                    id in torrents
+                    and re.search('class="pro_\S*free', state["sp_state"]) != None
+                ):
+                    torrents[id]["free"] = True
+                    free_end = re.search('<span title="(.+?)"', state["timeout"])
+                    torrents[id]["free_end"] = (
+                        None
+                        if free_end == None
+                        else time.mktime(
+                            time.strptime(free_end.group(1), "%Y-%m-%d %H:%M:%S")
+                        )
+                        - time.timezone
+                        - config["HDChina"]["timezone"] * 3600
+                    )
+        else:
+            raise Exception
+        time.sleep(1)
+    session.close()
     return {
         "[HDChina]" + id: torrent
         for id, torrent in torrents.items()
